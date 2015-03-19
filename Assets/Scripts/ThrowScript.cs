8b8ceed061a0fa64e0ac56cc3725b3ad3d2d0e87
Mitchell Sharp <mitchellsha@gmail.com>
2015-03-19 15:23:40 -0500

    Level 3 Complete
    Level 3 Added
    Game does not attempt to proceed after level 3
    Bone respawns

---
+++ Assets/Scripts/ThrowScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowScript : MonoBehaviour {
+	public GameObject bone;
+	public float strength;
+	private bool isHolding;
+	public Vector3 holdingOffset;
+
+	void Start () {
+		bone.transform.position = transform.position;
+		isHolding = true;
+	}
+
+	void Update () {
+		if (isHolding) {
+			//hold bone above the player, ready to throw
+			bone.transform.position = transform.position + holdingOffset;
+			bone.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+			if (Input.GetKey(KeyCode.F)) {
+				throwBone();
+			}
+		} else {
+			if(bone.transform.position.y < 0){
+				float x = bone.transform.position.x % 20f;
+				float z = bone.transform.position.z % 20f;
+				bone.transform.position = new Vector3(x, 10, z);
+			}
+		}
+	}
+
+	private void throwBone(){
+		Camera c = GetComponent<Camera>();
+		Vector3 throwForce = c.transform.forward;
+		bone.GetComponent<Rigidbody> ().AddForce (throwForce * strength);
+		isHolding = false;
+	}
+
+	void onReturn(){
+		Debug.Log ("got Bone");
+		isHolding = true;
+	}
+}
