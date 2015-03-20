5c3fc7149913f6c3043739059c837b123b2df9b9
Mark Christian Woodard <woodardmc@hendrix.edu>
2015-03-20 08:58:31 -0500

    Variable Throw Power
    added a variable forceMultiplier to the throwScript
    This variable tracks and affects throw strength
    First press of F starts forceMultiplier going up and down
    Second press of F throws the Bone
    Dog goes to fetch mode only when bone is thrown
    This branch is based off of Mitchell's level 3 commit.

---
+++ Assets/Scripts/DogScript.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DogScript : MonoBehaviour {
+	private NavMeshAgent agent;
+	private enum States {WAIT, FETCH, RETURN};
+	private int currentState, throwCount;
+	public GameObject target, human;
+	private bool goFetch = false;
+
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+
+		agent = GetComponent<NavMeshAgent>();
+		currentState = (int)States.WAIT;
+		throwCount = 0;
+
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F2)) {
+			RestartGame();
+		}
+
+		if(currentState == (int)States.WAIT){
+			Wait();
+		} else if(currentState == (int)States.FETCH){
+			Fetch();
+		} else if(currentState == (int)States.RETURN){
+			Return();
+		}
+	}
+	
+	void Wait(){
+		if(goFetch){
+			agent.destination = target.transform.position;
+			currentState = (int)States.FETCH;
+		} else if(Mathf.Abs((transform.position - human.transform.position).magnitude) > 2){
+			agent.destination = human.transform.position;
+		} else {
+			agent.destination = transform.position;
+		}
+	}
+	
+	void Fetch(){
+		if(Mathf.Abs((transform.position - target.transform.position).magnitude) < 2){
+			agent.destination = human.transform.position;
+			currentState = (int)States.RETURN;
+			goFetch = false;
+		} else {
+			agent.destination = target.transform.position;
+		}
+	}
+	
+	void Return(){
+		if(Mathf.Abs((transform.position - human.transform.position).magnitude) < 2){
+			currentState = (int)States.WAIT;
+			throwCount++;
+
+			//send a message saying that the bone has been returned
+			human.SendMessage("onReturn");
+
+			if(throwCount == 3){
+				int num = Application.loadedLevel + 2;
+				if(num <= 3){
+					Application.LoadLevel("level"+num);
+				} else {
+					//show game over screen
+				}
+			}
+		} else {
+			//hold the bone above the dog as its walking back
+			target.transform.position = transform.position + new Vector3(0, 2, 0);
+			agent.destination = human.transform.position;
+		}
+	}
+
+	void onBoneThrow(){
+		goFetch = true;
+	}
+
+	void RestartGame(){
+		Application.LoadLevel("level1");
+	}
+}
---
+++ Assets/Scripts/ThrowScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowScript : MonoBehaviour {
+	public GameObject bone;
+	public float strength;
+	public float lowerForceLimit;
+	public float forceBarSpeed;
+	private bool isHolding;
+	public Vector3 holdingOffset;
+	private float forceMultiplier = (float).3;
+	private bool firstPress = false;
+	private bool goingUp = true;
+
+	void Start () {
+		bone.transform.position = transform.position;
+		isHolding = true;
+	}
+
+	void Update () {
+		if (isHolding) {
+			//hold bone above the player, ready to throw
+			bone.transform.position = transform.position + holdingOffset;
+			bone.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
+			if (Input.GetKeyDown(KeyCode.F)) {
+				firstPress = !firstPress;
+				if(!firstPress){
+					throwBone();
+				}
+			}
+			if (firstPress){
+				goForceMeter ();
+			}
+		} else {
+			if(bone.transform.position.y < 0){
+				float x = bone.transform.position.x % 20f;
+				float z = bone.transform.position.z % 20f;
+				bone.transform.position = new Vector3(x, 10, z);
+			}
+		}
+	}
+
+	private void throwBone(){
+		Camera c = GetComponent<Camera>();
+		Vector3 throwForce = c.transform.forward;
+		bone.GetComponent<Rigidbody> ().AddForce (throwForce * (strength * forceMultiplier));
+		isHolding = false;
+		forceMultiplier = 0;
+		FindObjectOfType<DogScript> ().SendMessage ("onBoneThrow");
+	}
+
+	//forceMultiplier will bounce between lowerForceLimit and 1 at a rate of forceBarSpeed per frame 
+	private void goForceMeter(){
+		if(forceMultiplier > 1){
+			goingUp = false;
+		}
+		if (forceMultiplier < lowerForceLimit){
+			goingUp = true;
+		}
+		if (goingUp){
+			forceMultiplier = forceMultiplier + forceBarSpeed;
+		}else{
+			forceMultiplier = forceMultiplier - forceBarSpeed;
+		}
+	}
+
+	void onReturn(){
+		Debug.Log ("got Bone");
+		isHolding = true;
+	}
+}
